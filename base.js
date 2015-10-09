5e874592ff0f104df2f69a134b1cad14842c8ea9
Juliana <jlopker@gmail.com>
2015-10-09 13:21:04 -0700

    init

--- base.js
+++ base.js
@@ -1,7 +1,47 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+    var searchInput;
+    var api = 'https://api.spotify.com/v1/search?type=artist&q=';
+    var query;
+    var queryResults;
 
-  // code in here
+    $('#searchApi').on('click', function(e) {
+        e.preventDefault();
 
+        $('#artists').html("");
+
+        searchInput = $('#searchInput').val();
+
+        query = api + searchInput;
+
+        getApiData();
+
+        $('#searchInput').val("");
+    });
+
+    function getApiData() {
+        $.get(query, function(data){
+            console.log(data);
+
+            var items = data.artists.items;
+            var artistApi = data.artists.items[0].href;
+            var image = data.artists.items[0].images[0].url;
+            loopArtists(items);
+
+            $('#artists').append('<a href='+artistApi+'><img class="artistImage" src='+image+'></a>');
+        });
+    }
+
+    function loopArtists(items) {
+        for (var i=0; i<items.length; i++) {
+
+        }
+    }
+
+    function getTopTracks() {
+        $.get(artistApi+"/top-tracks?country=US", function() {
+            
+        });
+    }
 });
